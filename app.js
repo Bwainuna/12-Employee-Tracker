99757934e53cc1d0a9eb1dd726abd43314d5a7ea
Bwainuna <blakebah2@gmail.com>
2023-10-30 19:18:50 -0600

    added an inquirer function with prompts to guid the user to various access points in the database.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const consoleTable = require('console.table'); // For formatting and displaying tables
 
 // The database connection parameters
 const db = mysql.createConnection({
@@ -9,11 +10,63 @@ const db = mysql.createConnection({
   database: 'employee_db',
 });
 
-// Connects to the database
+// Connect to the database
 db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.message);
-    return;
+    if (err) {
+      console.error('Error connecting to the database: ' + err.message);
+      return;
+    }
+    console.log('Connected to the database');
+    startApp();
+  });
+  
+  // Define the main application function
+  function startApp() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'menuChoice',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.menuChoice) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            db.end();
+            console.log('Disconnected from the database');
+            break;
+        }
+      });
   }
-  console.log('Connected to the database');
-});
